000cf00f6df357f35a8478cb6e913f22ce87161a
dtraft <dtraft@gmail.com>
2014-07-18 18:10:27 -0400

    (#21) - remove attachments when copying components

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -309,6 +309,11 @@ angular.module('starter.controllers', [])
 		  				//clean up component
 		  				delete component._id;
 		  				delete component._rev;
+						if(component._attachments){
+							delete component._attachments;
+						}
+						component.forTemplate = false;
+						
 		  				//assign new projectId
 		  				component.projectId = newId;
 						component.created = today.toISOString();
@@ -634,6 +639,8 @@ angular.module('starter.controllers', [])
 		//strip db identifiers
 		delete copy._id;
 		delete copy._rev;
+		delete copy._attachments;
+		
 		//strip out digits
 		copy.name = copy.name.replace(/(\d+)/g,"");
 	    //more regex
@@ -876,6 +883,8 @@ angular.module('starter.controllers', [])
 			  //clean up component
 			  delete component._id;
 			  delete component._rev;
+			  delete component._attachments;
+			  
 	  		  //strip functions
 	  		  var functions = _.functions(component);
 	  		  for(var f = 0; f < functions.length; f++){
@@ -1216,7 +1225,7 @@ angular.module('starter.controllers', [])
 		  				//clean up component
 		  				delete component._id;
 		  				delete component._rev;
-						component.forTemplate = false;
+						component.forTemplate = true;
 						
 						//set up dates
 						now.setSeconds(now.getSeconds() + 1);
@@ -1528,6 +1537,7 @@ angular.module('starter.controllers', [])
 		   	  newItem.channels = ["public"];
 		   	  newItem.projectId = $scope.project._id;
 			  newItem.values = {};
+			  newItem.forTemplate = true;
   
 		   	  //move these forward for easy access
 		   	  newItem.libraryId = newItem.schema.libraryId;
